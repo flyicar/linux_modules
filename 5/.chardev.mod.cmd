1fcc6ba2d3c7876b87d952503528a76cd6da5fc0
flyicar <ggopack078@gmail.com>
2022-12-17 23:07:18 +0700

    added directory 5 with files: chardev.c Makefile readme.md

---
+++ 5/.chardev.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/flyicar/eltex/mod5/linux_modules/5/chardev.mod := { echo  /home/flyicar/eltex/mod5/linux_modules/5/chardev.o;  echo; } > /home/flyicar/eltex/mod5/linux_modules/5/chardev.mod
---
+++ 5/readme.md
@@ -0,0 +1,3 @@
+# Eltex, task 5
+
+A simple linux module for exchanging hello string using chardev 
